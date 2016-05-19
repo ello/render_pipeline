5838376bff7f7f8512a2077bbdd26ca86ded059b
Justin-Holmes <justin.ryan.holmes@icloud.com>
2016-05-19 14:48:08 -0600

    Use absolute links over relative in emails

--- lib/render_pipeline/filters/link_adjustments.rb
+++ lib/render_pipeline/filters/link_adjustments.rb
@@ -3,13 +3,20 @@ module RenderPipeline
     class LinkAdjustments < HTML::Pipeline::Filter
       def call
         host_regex = %r{\Ahttp(s)?://(www.)?#{context[:host_name]}(/.*)}
-        doc.search('a').each do |element|
+          doc.search('a').each do |element|
           element['href'] ||= ''
+
+          if context[:notification_email]
+            href = "https://#{context[:host_name]}" + element['href']
+            element['href'] = CGI.unescapeHTML(href)
+          end
 
           # make relative links.
           # todo: move this regexp into configuration
-          if element['href'] =~ host_regex
-            element['href'] = CGI.unescapeHTML($3)
+          unless context[:notification_email]
+            if element['href'] =~ host_regex
+              element['href'] = CGI.unescapeHTML($3)
+            end
           end
 
           # adjust links that are not relative.
