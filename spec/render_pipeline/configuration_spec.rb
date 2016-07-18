6e0c59e2414f3fed0aa9be609b97c6c1f0989375
Jay Zeschin <jay.zeschin@modeset.com>
2016-07-18 13:43:34 -0600

    Allow multiple names to be specified for a single context
    Makes it easier to effectively alias contexts.
    
    Also add pry for debugging tests

--- spec/render_pipeline/configuration_spec.rb
+++ spec/render_pipeline/configuration_spec.rb
@@ -9,6 +9,32 @@ describe RenderPipeline::Configuration do
     RenderPipeline.configuration.sanitize_rules = old
   end
 
-  it 'allows configuring contexts'
+  describe 'configuring contexts' do
+    it 'configures a default context when no name is provided' do
+      RenderPipeline.configure do |config|
+        config.render_context do |c|
+          c.host_name = 'example.com'
+        end
+      end
+      default_context = RenderPipeline.configuration.render_contexts['default'][:instance]
+      expect(default_context.host_name).to eq('example.com')
+    end
 
+    it 'configures a named context when a name is provided' do
+      RenderPipeline.configure do |c|
+        c.render_context :nondefault do
+        end
+      end
+      expect(RenderPipeline.configuration.render_contexts).to have_key('nondefault')
+    end
+
+    it 'configures multiple identical named contexts when multiple names are provided' do
+      RenderPipeline.configure do |c|
+        c.render_context :nondefault, :extranondefault do
+        end
+      end
+      expect(RenderPipeline.configuration.render_contexts).to have_key('nondefault')
+      expect(RenderPipeline.configuration.render_contexts).to have_key('extranondefault')
+    end
+  end
 end
